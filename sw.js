502763c1166005b23496f1c4ae07278a5999387d
DiegoJesuP <spartfomi@outlook.com>
2025-11-20 16:40:40 -0600

    [FIRST COMMIT] primer commit del proyecto

---
+++ sw.js
@@ -0,0 +1,10 @@
+
+self.addEventListener("install", (event) => {
+  console.log("SW: instalado");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("SW: activado");
+  return self.clients.claim();
+});
